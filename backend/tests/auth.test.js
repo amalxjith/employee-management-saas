1db572e3df1d40fc2c522c60cda58b998246a67f
amaljith <amaljithamalu45@gmail.com>
2026-05-27 22:30:30 +0530

    Test changes

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -1,13 +1,16 @@
 const request = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../src/app");
-const connectDB = require("../src/config/db");
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config({
+  path: ".env.test",
+});
 
 beforeAll(async () => {
-  await connectDB();
+  await mongoose.connect(
+    process.env.TEST_MONGO_URI
+  );
 });
 
 afterAll(async () => {
@@ -22,21 +25,6 @@ describe("Auth API", () => {
     });
 
     expect(response.statusCode).toBe(401);
-  }, 10000);
 
-  test("Should register new user", async () => {
-    const response = await request(app).post("/api/auth/register").send({
-      name: "Amal",
-      email: "amal1@test.com",
-      password: "123456",
-    });
-
-    console.log(response,'lmmmm');
-
-    expect(response.statusCode).toBe(201);
-
-    expect(response.body).toHaveProperty("token");
-
-    expect(response.body.email).toBe("amal1@test.com");
-  }, 15000);
+  },10000);
 });
